f5fc98b47f4ba4b158e65905054de40db9c58194
shivam tyagi <60812924+ShivamTyagi12345@users.noreply.github.com>
2021-03-13 23:07:42 +0530

    footer added

--- src/App.js
+++ src/App.js
@@ -3,17 +3,27 @@ import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-d
 import Auth from './components/auth/Auth';
 import Main from './components/main/Main';
 import './App.css';
+ import  { FooterContainer }from './containers/footer';
+
+
+
 
 function App() {
   return (
     <div className="App">
+  
       <Router>
         <Switch>
+
           <Route path='/auth' component={Auth} />
           <Route path='/main' component={Main} />
           <Redirect to='/auth' from='*' />
+          
         </Switch>
+       
       </Router>
+
+      <FooterContainer/>
     </div>
   );
 }
