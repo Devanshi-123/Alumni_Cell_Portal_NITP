f997026810413894498e0cdb97bcfef1907f8604
samriddhiambashta <ambashtasamriddhi@gmail.com>
2021-03-15 19:54:12 +0530

    Adding about us page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Auth from './components/auth/Auth';
 import Main from './components/main/Main';
+import About from './components/about-us/about';
 import './App.css';
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
       <Router>
         <Switch>
           <Route path='/auth' component={Auth} />
-          <Route path='/main' component={Main} />
+                  <Route path='/main' component={Main} />
+                  <Route path='/about' component={About} />
           <Redirect to='/auth' from='*' />
         </Switch>
       </Router>
