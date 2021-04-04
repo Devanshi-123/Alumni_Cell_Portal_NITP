ff4e7e385083f62c0ddb5f99509c71732c85ebea
Nikhil Ranjan Kumar <52556284+all0fme@users.noreply.github.com>
2021-04-04 20:42:06 +0530

    Revert "Navbar made responsive"
    This reverts commit e0653cefa12b22b703041ca6a60b09372712583c.

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -4,17 +4,17 @@ import "./Nav.css"
 function Nav() {
     return (<div className="container-fluid">
         <div className="row">
-            <div className="col-md-6 col-12 logo">
+            <div className="col-6">
                 <h1 className="thicker">ALUMNI <span className="cell">CELL</span></h1>
                 <p className="thick">National Institute Of Technology Patna</p>
             </div>
-            <div className="col-md-2 col-4 orange-box box ">
+            <div className="col-2 orange-box box ">
                 <a href="#">Menu</a>
             </div>
-            <div className="col-md-2 col-4 yellow-box box">
+            <div className="col-2 yellow-box box">
                 <a href="#">Register</a>
             </div>
-            <div className="col-md-2 col-4 black-box box">
+            <div className="col-2 black-box box">
                 <a href="#">Contact Us</a>
             </div>
 
