6805c3a7cd03f6c5f1a96e2ce6f1edb1c702e1b7
Devanshi-123 <devanshi0608@gmail.com>
2021-04-04 19:17:44 +0530

    Deploying and routing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Auth from './components/auth/Auth';
 import Main from './components/main/Main';
 import About from './components/about-us/about';
 import Story from './components/stories/Story';
+import Footer from "./components/footer/Footer";
 import Event from './components/event/Event';
 import Core from './components/core/Core';
 import Home from './components/home/Home';
@@ -16,22 +17,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Nav />
-
       <Router>
+      <Nav />
         <Switch>
-          <Route path='/auth' component={Auth} />
-                  <Route path='/main' component={Main} />
-                  <Route path='/stories' component={Story} />
-          <Route path='/main' component={Main} />
-          <Route path='/about' component={About} />
-          <Route path='/event' component={Event} />
-          <Route path='/core' component={Core} />
-          <Route path='/home' component={Home} />
-          <Redirect to='/auth' from='*' />
+        <Route path='/' exact component={Home} />
+        <Route path='/auth' exact component={Auth} />
+        <Route path='/main' component={Main} />
+        <Route path='/stories' component={Story} />
+        <Route path='/main' component={Main} />
+        <Route path='/about' component={About} />
+        <Route path='/event' component={Event} />
+        <Route path='/core' component={Core} />
+        { /* <Redirect to='/auth' from='*' />*/}
           
         </Switch>
-       
+       <Footer/>
       </Router>
 
 
