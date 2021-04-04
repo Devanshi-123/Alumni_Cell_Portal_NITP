8d2f1ea3f837f552aa8c2f955c4fe314ddfd0804
shivam tyagi <60812924+ShivamTyagi12345@users.noreply.github.com>
2021-04-04 15:54:40 +0530

    write to us section

---
+++ src/components/home/MailUs/App2.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Contact from "./Contact";
+import "./app2.css";
+
+function App2() {
+  return (
+    <div className="app2">
+      <Contact />
+    </div>
+  );
+}
+
+export default App2;
