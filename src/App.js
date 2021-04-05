db017e419ef84187f97c71cd08b70e3f93b80238
PARITOSH KUMAR <code.paritosh@outlook.com>
2021-04-05 17:55:46 +0530

    Added route path for NewEvent page

--- src/App.js
+++ src/App.js
@@ -6,35 +6,35 @@ import About from './components/about-us/about';
 import Story from './components/stories/Story';
 import Footer from "./components/footer/Footer";
 import Event from './components/event/Event';
+import NewEvent from './components/event/newEvent/NewEvent';
 import Core from './components/core/Core';
 import Home from './components/home/Home';
 import Nav from "./components/nav/Nav";
 import './App.css';
 
 
-
-
 function App() {
   return (
     <div className="App">
       <Router>
-      <Nav />
+
+        <Nav />
+
         <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/auth' exact component={Auth} />
-        <Route path='/main' component={Main} />
-        <Route path='/stories' component={Story} />
-        <Route path='/main' component={Main} />
-        <Route path='/about' component={About} />
-        <Route path='/event' component={Event} />
-        <Route path='/core' component={Core} />
-        { /* <Redirect to='/auth' from='*' />*/}
-          
+          <Route path='/' exact component={Home} />
+          <Route path='/auth' exact component={Auth} />
+          <Route path='/main' component={Main} />
+          <Route path='/stories' component={Story} />
+          <Route path='/about' component={About} />
+          <Route path='/core' component={Core} />
+          <Route path='/event/new' component={NewEvent} />
+          <Route path='/event' component={Event} />
+          { /* <Redirect to='/auth' from='*' />*/}
         </Switch>
-       <Footer/>
+
+        <Footer />
+
       </Router>
-
-
     </div>
   );
 }
